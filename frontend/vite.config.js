fb78df4b36aec89b5bb5afd1875a95804a9f89b4
yeasin097 <yeasin.cse19@gmail.com>
2025-05-31 14:58:48 +0600

    config2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: '0.0.0.0', // Listen on all interfaces
-//     port: 5173,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         changeOrigin: true,
-//         secure: false
-//       },
-//     },
-//     hmr: {
-//       protocol: 'ws',
-//       host: 'localhost', // Base domain for HMR
-//     },
-//     allowedHosts: [
-//       'all',
-//       '.poridhi.io',      // All subdomains of poridhi.io
-//       '.lab.poridhi.io',  // All subdomains of lab.poridhi.io
-//     ],
-//   },
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -34,18 +5,47 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Required to expose to Docker
+    host: '0.0.0.0', // Listen on all interfaces
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://backend:5000', // Docker service name
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false,
+        secure: false
       },
     },
     hmr: {
       protocol: 'ws',
-      host: 'localhost', // Ensure HMR works on local browser
+      host: 'localhost', // Base domain for HMR
     },
+    allowedHosts: [
+      'all',
+      '.poridhi.io',      // All subdomains of poridhi.io
+      '.lab.poridhi.io',  // All subdomains of lab.poridhi.io
+    ],
   },
 });
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0', // Required to expose to Docker
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://backend:5000', // Docker service name
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//     hmr: {
+//       protocol: 'ws',
+//       host: 'localhost', // Ensure HMR works on local browser
+//     },
+//   },
+// });
