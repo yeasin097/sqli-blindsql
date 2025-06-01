1307f17cc4d622c358ebbff52ad052b3d246e031
yeasin097 <yeasin.cse19@gmail.com>
2025-06-01 11:33:41 +0600

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://678aa840859cc728c0ad9211-lb-740.bm-south.lab.poridhi.io/',
+        target: 'http://localhost:5000/',
         changeOrigin: true,
       },
     },
